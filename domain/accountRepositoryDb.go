d7ab43aa75ce1537419f8fdb5724d8116c9be8b5
nickypangers <nixonpang98@gmail.com>
2022-05-02 18:43:38 +0800

    fixed savetransaction not saving

--- domain/accountRepositoryDb.go
+++ domain/accountRepositoryDb.go
@@ -50,44 +50,54 @@ func (d AccountRepositoryDb) Save(a Account) (*Account, *errs.AppError) {
 }
 
 func (d AccountRepositoryDb) SaveTransaction(t Transaction) (*Transaction, *errs.AppError) {
+	// starting the database transaction block
 	tx, err := d.client.Begin()
 	if err != nil {
-		logger.Error("Error while starting a new transaction: " + err.Error())
-		return nil, errs.NewUnexpectedNotFoundError("Unexpected server error")
+		logger.Error("Error while starting a new transaction for bank account transaction: " + err.Error())
+		return nil, errs.NewUnexpectedNotFoundError("Unexpected database error")
 	}
 
-	result, _ := tx.Exec("INSERT INTO transactions (account_id, amount, transaction_type, transaction_date) values (?, ?, ?, ?)", t.AccountId, t.Amount, t.TransactionType, t.TransactionDate)
-	if err != nil {
-		logger.Error("Error while saving transaction " + err.Error())
-		return nil, errs.NewUnexpectedNotFoundError("Unexpected server error")
-	}
+	// inserting bank account transaction
+	result, _ := tx.Exec(`INSERT INTO transactions (account_id, amount, transaction_type, transaction_date) 
+											values (?, ?, ?, ?)`, t.AccountId, t.Amount, t.TransactionType, t.TransactionDate)
+
+	// updating account balance
 	if t.IsWithdrawal() {
-		_, err = tx.Exec("UPDATE accounts SET amount = amount - ? WHERE account_id = ?", t.Amount, t.AccountId)
+		_, err = tx.Exec(`UPDATE accounts SET amount = amount - ? where account_id = ?`, t.Amount, t.AccountId)
 	} else {
-		_, err = tx.Exec("UPDATE accounts SET amount = amount + ? WHERE account_id = ?", t.Amount, t.AccountId)
+		_, err = tx.Exec(`UPDATE accounts SET amount = amount + ? where account_id = ?`, t.Amount, t.AccountId)
 	}
+
+	// in case of error Rollback, and changes from both the tables will be reverted
 	if err != nil {
 		tx.Rollback()
-		logger.Error("Error while saving transaction " + err.Error())
-		return nil, errs.NewUnexpectedNotFoundError("Unexpected server error")
+		logger.Error("Error while saving transaction: " + err.Error())
+		return nil, errs.NewUnexpectedNotFoundError("Unexpected database error")
 	}
-
+	// commit the transaction when all is good
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		logger.Error("Error while commiting transaction for bank account: " + err.Error())
+		return nil, errs.NewUnexpectedNotFoundError("Unexpected database error")
+	}
+	// getting the last transaction ID from the transaction table
 	transactionId, err := result.LastInsertId()
 	if err != nil {
-		logger.Error("Error while getting last insert transaction id " + err.Error())
-		return nil, errs.NewUnexpectedNotFoundError("Unexpected server error")
+		logger.Error("Error while getting the last transaction id: " + err.Error())
+		return nil, errs.NewUnexpectedNotFoundError("Unexpected database error")
 	}
 
+	// Getting the latest account information from the accounts table
 	account, appErr := d.ById(t.AccountId)
 	if appErr != nil {
 		return nil, appErr
 	}
-
 	t.TransactionId = strconv.FormatInt(transactionId, 10)
 
+	// updating the transaction struct with the latest balance
 	t.Amount = account.Amount
 	return &t, nil
-
 }
 
 func NewAccountRepositoryDb(dbClient *sqlx.DB) AccountRepositoryDb {
